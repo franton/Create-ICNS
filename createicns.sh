079750c11ede747f91750722ef852f4fc5e7b5ac
Richard Purves <franton@users.noreply.github.com>
2016-06-24 10:36:15 +0200

    Update createicns.sh
    Improved error checking

--- createicns.sh
+++ createicns.sh
@@ -3,17 +3,43 @@
 # Script to process .png into an .icns file
 # .png to process must be 1024x1024 at 144 dpi or this fails hard.
 
+# Has a file actually been submitted?
+
 if [ "$1" = "" ];
 then
 		echo "Usage: ./createicns.sh <full path to source png>"
-		echo "Your .png file must be 1024x1024 resolution at 144 dpi."
 		exit 1
 fi
+
+# Work out file, path and extension info
 
 fullpath="$1"
 path=$( dirname $1 )
 ext=${1##*.}
 file=$( basename $1 .$ext )
+
+# Is this a .png file?
+
+if [ "$ext" != "png" ];
+then
+	echo "Please use a .png file with this utility"
+	exit 1
+fi
+
+# Is the .png file the correct size and dpi?
+
+dpix=$( sips --getProperty dpiWidth "$fullpath" | awk '{if (NR =2) print $2 }' | cut -d . -f 1 )
+dpiy=$( sips --getProperty dpiHeight "$fullpath" | awk '{if (NR =2) print $2 }' | cut -d . -f 1 )
+pixelx=$( sips --getProperty pixelWidth "$fullpath" | awk '{if (NR =2) print $2 }' | cut -d . -f 1 )
+pixely=$( sips --getProperty pixelHeight "$fullpath" | awk '{if (NR =2) print $2 }' | cut -d . -f 1 )
+
+if [ "$dpix" != "144" ] || [ "$dpiy" != "144" ] || [ "$pixelx" != "1024" ] || [ "$pixely" != "1024" ];
+then
+	echo "Your .png file must be 1024x1024 resolution at 144 dpi."
+	exit 1
+fi
+
+# Create the icns file
 
 mkdir "$path"/"$file".iconset
 
